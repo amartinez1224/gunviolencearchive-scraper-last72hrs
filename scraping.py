4f9c1200335d966c110098dd21c04278929f379e
Andres Martinez <44838230+amartinez1224@users.noreply.github.com>
2022-09-04 14:21:01 -0400

    Updated scraping into functions

--- scraping.py
+++ scraping.py
@@ -4,14 +4,19 @@ import asyncio
 import random
 
 URL = 'https://www.gunviolencearchive.org/last-72-hours'
- 
-async def main():
+
+def writeData(tables, fileName = 'data/dataTable.txt'):
+    with open(fileName, 'w+') as f:
+        for table in tables:
+            f.write(table)
+
+async def scrape(waitTime = 2, timeout = 2000):
+    tables = []
     async with async_playwright() as p:
         browser = await p.firefox.launch()
         page = await browser.new_page()
         await page.goto(URL, wait_until="networkidle")
 
-        tables = []
         while True:
             tableElement = page.locator('tbody')
             table = await tableElement.inner_html()
@@ -19,17 +24,18 @@ async def main():
 
             next = page.locator("a[title='Go to next page']").nth(0)
             try:
-                await next.wait_for(state='visible',timeout=2000)
+                await next.wait_for(state='visible',timeout=timeout)
             except TimeoutError:
                 break
-            await asyncio.sleep(random.random()*3)
+            await asyncio.sleep(random.random()*waitTime)
             await next.click()
 
         await browser.close()
+    return tables
 
-        with open('data/dataTable.txt', 'w+') as f:
-            for table in tables:
-                f.write(table)
+async def main():
+    tables = await scrape()
+    writeData(tables)
 
 if __name__ == '__main__':
     asyncio.run(main())
